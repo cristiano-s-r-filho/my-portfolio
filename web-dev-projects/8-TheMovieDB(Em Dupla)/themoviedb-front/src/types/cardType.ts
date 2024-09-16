a2cce168a43284a8511d234b1111d77387e3e820
cristiano-s-r-filho <cristiano.sr.filho@proton.me>
2024-09-15 22:23:00 -0300

    O commit devia ter sido feito aqui, duh

--- web-dev-projects/8-TheMovieDB(Em Dupla)/themoviedb-front/src/types/cardType.ts
+++
@@ -1,9 +0,0 @@
-export type cardType = {
-    adult: boolean;
-    backdrop_path: string;
-    id: number;
-    original_language: string;
-    original_title: string;
-    overview: string;
-    vote_average: number;
-}
